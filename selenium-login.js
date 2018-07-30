296cf2012072816b962af47f8d3ba3077903c4ab
Ocean5app <ocean5app@outlook.com>
2018-07-30 23:51:11 +0800

    Update selenium-login.js

--- selenium-login.js
+++ selenium-login.js
@@ -1,3 +1,8 @@
+/*
+用于自动登录service now 网站的测试脚本， 需要预先安装 node.js 和 selenium-webdriver
+确保在注册表的如下位置有MachineLevelUserCloudPolicyEnrollmentToken文件（ 值为空值）
+HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome
+*/
 console.time("driver");
 var webdriver = require('selenium-webdriver'),
     By = webdriver.By,
